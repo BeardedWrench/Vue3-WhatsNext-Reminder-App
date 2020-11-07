cb6ce64ebb0115441be2f63c312eddb4868e22dd
Terry Brown <tabrownj@gmail.com>
2020-11-07 10:26:26 -0500

    Basic reminder app
    A very simple reminder app built with Vue3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <header><img src="./images/logo.png" class="logo">What's Next?</header>
+<div id="container">
+    <div id="input-fields">
+        <form @submit.prevent="addNewReminder">
+            <input type="text" placeholder="Business meeting with bob..." class="reminder-input" v-model="newReminder" name="newTodo">
+            <button type="submit" class="btn submit">Submit</button>
+        </form>
+    </div>
+    <h2>Upcoming Reminders</h2>
+    <div id="reminder-container">
+       <ul class="reminder">
+        <li v-for="(reminder, index) in reminders" :key="reminder.id" class="reminder-text">{{reminder.content}} - <button class="btn blue">Edit</button><button class="btn red" @click="removeReminder(index)">Delete</button></li>
+       </ul>
+    </div>
+</div>
+    <footer>Created by <a href="#">BeardedWrench</a> - &copy; 2020</footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const newReminder = ref('');
+    const reminders = ref([]);
+    function addNewReminder() {
+      reminders.value.push({
+        id: Date.now(),
+        done: false,
+        content: newReminder.value,
+      });
+      newReminder.value = '';
+    }
+    function toggleDone(reminder) {
+      reminder.done = !reminder.done;
+    }
+    function removeReminder(index) {
+      reminders.value.splice(index, 1);
+    }
+    return {
+      reminders,
+      newReminder,
+      addNewReminder,
+      toggleDone,
+      removeReminder,
+    };
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+require('./styles.css');
+
 
 createApp(App).mount('#app')
